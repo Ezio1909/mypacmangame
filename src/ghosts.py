f9e78b0a56fc4035b873a23c42b12a068f6bda56
Nam Pham <nam.pham1@mservice.com.vn>
2025-01-12 18:09:05 +0700

    add two moving move for ghost

--- src/ghosts.py
+++ src/ghosts.py
@@ -3,11 +3,28 @@ from pygame.locals import *
 from constants import *
 from vector import Vector
 from entity import Entity
+from modes import ModeController
 
 class Ghost(Entity):
-    def __init__(self, node):
+    def __init__(self, node, pacman=None):
         Entity.__init__(self, node)
         self.name = GHOST
         self.points = 200
         self.goal = Vector()
         self.directionMethod = self.goalDirection
+        self.pacman = pacman
+        self.mode = ModeController(self)
+
+    def update(self, dt):
+        self.mode.update(dt)
+        if self.mode.current is SCATTER:
+            self.scatter()
+        elif self.mode.current is CHASE:
+            self.chase()
+        Entity.update(self, dt)
+
+    def scatter(self):
+        self.goal = Vector()
+
+    def chase(self):
+        self.goal = self.pacman.position
